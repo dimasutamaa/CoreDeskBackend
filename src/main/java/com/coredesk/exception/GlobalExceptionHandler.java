18090ffa166e2821b9985853853cbf7a211d82a2
dimasutamaa <dimasutamapa@gmail.com>
2026-03-09 17:38:37 +0700

    feat(exception): add logger for debugging purpose

--- src/main/java/com/coredesk/exception/GlobalExceptionHandler.java
+++ src/main/java/com/coredesk/exception/GlobalExceptionHandler.java
@@ -1,24 +1,28 @@
 package com.coredesk.exception;
 
 import com.coredesk.dto.RestResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AppException.class)
     public ResponseEntity<RestResponse> handleAppException(AppException e) {
+        log.warn(e.getMessage());
         return ResponseEntity.status(e.getStatus())
                 .body(new RestResponse(e.getStatus().toString(), e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public RestResponse handleException(Exception e) {
-        return new RestResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage());
+    public ResponseEntity<RestResponse> handleException(Exception e) {
+        log.error("Unexpected error occurred", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new RestResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Internal server error"));
     }
 }
